8941c724b9d74116eac3a29190f0c13100e1f19f
Eduardo Rosales <eduardo_rosales@eduardos-mbp-2.dynamic.ucsd.edu>
2016-04-19 17:18:24 -0700

    test_CI

--- app/src/androidTest/java/com/example/eduardo_rosales/lab3exercise/tests/JUnit_test.java
+++ app/src/androidTest/java/com/example/eduardo_rosales/lab3exercise/tests/JUnit_test.java
@@ -35,4 +35,28 @@ public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity> {
         btnAdd = (Button)findViewById(R.id.btnAdd);
         */
     }
+
+    public void test_Second() {
+        mainActivity = getActivity();
+
+        EditText sum = (EditText)mainActivity.findViewById(com.example.eduardo_rosales.lab3exercise.R.id.txtResult);
+
+        double number1 = 6.0;
+        double number2 = 8.0;
+
+        //com.example.eduardo_rosales.lab3exercise.R.id.txtNumber1
+
+        double num1 = com.example.eduardo_rosales.lab3exercise.R.id.txtNumber1;
+        double num2 = com.example.eduardo_rosales.lab3exercise.R.id.txtNumber2;
+        double sumNum = num1 + num2;
+
+        assertEquals(sumNum, sumNum);
+
+        /*
+        firstNumber = (EditText)findViewById(R.id.txtNumber1);
+        secondNumber = (EditText)findViewById(R.id.txtNumber2);
+        addResult = (TextView)findViewById(R.id.txtResult);
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+        */
+    }
 }
